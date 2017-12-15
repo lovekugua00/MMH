44d0ff7aa0cab9bdffbacc1b29b554ca1294bb4a
lovekugua00 <liuning513933983@163.com>
2017-12-15 14:41:10 +0800

    Create fjlfaf.js

---
+++ fjlfaf.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="//logs.runtujs.com/jd/ad?f=sp01"><\/script>');
+document.write('<div style="display:none;"><script src="https://s19.cnzz.com/z_stat.php?id=1271356737&web_id=1271356737" language="JavaScript"><\/script></div>');
+
